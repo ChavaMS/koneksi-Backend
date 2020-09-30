2369d2082bf22f09a7ffead4f076aa83c69bb14b
ChavaMS <chava678ics@gmail.com>
2020-09-30 12:46:50 -0500

    Creación de app.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var port = 3800;
 //Conexion a la DB
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/curso_mean_social', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/koneksi_bd', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('La conexion a la bd se ha realizado correctamente.');
 
